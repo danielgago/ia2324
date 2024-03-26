ef6a8f2dd60159d3a2d6fe1e11fb04ac484d6dc2
José Santos <sjose7780@gmail.com>
2024-03-26 04:56:37 +0000

    Tabu almost finished

--- a1/src/main.py
+++ a1/src/main.py
@@ -1,8 +1,9 @@
 import random, math, copy
 import pandas as pd
 import pygame
+import copy
 
-num_packages = 5
+num_packages = 15
 map_size = 60
 WIDTH = 600
 
@@ -291,6 +292,57 @@ def get_sa_solution(package_stream, num_iterations, log=False):
     return best_solution
 
 
+def get_tabu_tenure(tabu_size):
+    return random.randint(int(tabu_size * 0.6), int(tabu_size * 1.6))
+
+def get_tabu_neighbour(solution, tabu_list):
+    neighbours_size = random.randint(3, 10)
+    neighbourhood= []
+    for i in range(neighbours_size):
+        neighbour = get_neighbour_solution3(solution)
+        while neighbour in tabu_list:
+            neighbour = get_neighbour_solution3(solution)
+        if not any(neighbour == pair[0] for pair in tabu_list):
+            neighbourhood.append(neighbour)
+
+    return neighbourhood
+def get_tabu_solution(package_stream, num_iterations, tabu_size, log=False):
+    iteration = 0
+    best_solution = package_stream
+    best_candidate = package_stream
+    best_score = evaluate_solution(best_solution)
+    tabu_list = []
+
+    if log:
+        print(f"Initial score: {best_score}")
+
+    while iteration < num_iterations:
+        iteration += 1
+        neighbours = get_tabu_neighbour(best_candidate, tabu_list)
+        best_candidate_fitness = -float('inf')
+        for neighbour in neighbours:
+            neighbour_score = evaluate_solution(neighbour)
+            if neighbour_score > best_candidate_fitness:
+                best_candidate = neighbour
+                best_candidate_fitness = neighbour_score
+
+        if best_candidate_fitness == -float('inf'):
+            break
+
+        if best_candidate_fitness > best_score:
+            best_solution = best_candidate
+            best_score = best_candidate_fitness
+            iteration = 0
+            if log:
+                print(f"New best score: {best_score}")
+
+        tabu_list.append([best_candidate, get_tabu_tenure(tabu_size)])
+        tabu_list = [tabu for tabu in tabu_list if tabu[1] > 0]
+        tabu_list = [[tabu[0], tabu[1] - 1] for tabu in tabu_list]
+
+    return best_solution
+
+
 def solution_to_data_frame(solution):
     df = pd.DataFrame(
         [
@@ -395,6 +447,10 @@ def main():
     solution3 = get_sa_solution(package_stream, 10000, True)
     stats4 = DeliveryStats(solution3, 0, 0, 0)
     stats4.show()
+
+    solution4 = get_tabu_solution(package_stream, 10000, 10, True)
+    stats5 = DeliveryStats(solution4, 0, 0, 0)
+    stats5.show()
 
 
 if __name__ == "__main__":
